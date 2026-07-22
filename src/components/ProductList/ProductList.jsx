1d50c676cf3fbbe8cfa2ebd12e28054c5d73c1b9
Perfi <raphaelperfi@tutanota.com>
2026-07-22 19:27:07 -0300

    feat: add ProductList component styles

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,12 +1,14 @@
 import Product from "../Product/Product";
 
+import styles from "./ProductList.module.css";
+
 function ProductList() {
   return (
-    <section>
-      <h1>Desserts</h1>
+    <section className={styles.products}>
+      <h1 className={`${styles.title} text-preset-1`}>Desserts</h1>
 
-      <ul>
-        <li>
+      <ul className={styles.list}>
+        <li className={styles.item}>
           <Product />
         </li>
       </ul>
