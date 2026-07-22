a85e88db2d803bb2c29fc8b2ec3974d310cce93e
Perfi <raphaelperfi@tutanota.com>
2026-07-22 19:14:07 -0300

    feat: create ProductList component

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,17 @@
+import Product from "../Product/Product";
+
+function ProductList() {
+  return (
+    <section>
+      <h1>Desserts</h1>
+
+      <ul>
+        <li>
+          <Product />
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default ProductList;
