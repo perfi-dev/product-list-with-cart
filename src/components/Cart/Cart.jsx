8459d95c8c6d64e68bb55bcb628ee821ed1a904a
Perfi <raphaelperfi@tutanota.com>
2026-07-22 19:33:20 -0300

    feat: create Cart component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,14 @@
+function Cart() {
+  return (
+    <aside>
+      <h2>Your Cart (0)</h2>
+
+      <div>
+        <img src="/assets/images/illustration-empty-cart.svg" alt="" />
+        <p>Your added items will appear here</p>
+      </div>
+    </aside>
+  );
+}
+
+export default Cart;
