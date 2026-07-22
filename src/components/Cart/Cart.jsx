8bc0bb55c5aa3b28856be83781906fe35534b4e7
Perfi <raphaelperfi@tutanota.com>
2026-07-22 20:03:19 -0300

    feat: add Cart component styles

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,19 @@
+import styles from "./Cart.module.css";
+
 function Cart() {
   return (
-    <aside>
-      <h2>Your Cart (0)</h2>
+    <aside className={styles.cart}>
+      <h2 className={`${styles.title} text-preset-2`}>Your Cart (0)</h2>
 
-      <div>
-        <img src="/assets/images/illustration-empty-cart.svg" alt="" />
-        <p>Your added items will appear here</p>
+      <div className={styles.content}>
+        <img
+          src="/assets/images/illustration-empty-cart.svg"
+          alt=""
+          className={styles.image}
+        />
+        <p className={`${styles.text} text-preset-4-bold`}>
+          Your added items will appear here
+        </p>
       </div>
     </aside>
   );
