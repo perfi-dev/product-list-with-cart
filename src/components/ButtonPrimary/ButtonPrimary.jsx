5cc9684e4ed3d5df5c62b292132b731bb548626b
Perfi <raphaelperfi@tutanota.com>
2026-07-22 20:41:55 -0300

    feat: add ButtonPrimary component styles

--- src/components/ButtonPrimary/ButtonPrimary.jsx
+++ src/components/ButtonPrimary/ButtonPrimary.jsx
@@ -1,5 +1,9 @@
+import styles from "./ButtonPrimary.module.css";
+
 function ButtonPrimary({ children }) {
-  return <button>{children}</button>;
+  return (
+    <button className={`${styles.btnPrimary} text-preset-3`}>{children}</button>
+  );
 }
 
 export default ButtonPrimary;
